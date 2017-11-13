c7eeb5b980bbea2e899155c7b65d7a3f24a5d219
peaisge <raphi.lescot@hotmail.fr>
2017-11-13 19:10:08 +0100

    Update sigmoidGradient.py

--- sigmoidGradient.py
+++ sigmoidGradient.py
@@ -5,12 +5,7 @@ def sigmoidGradient(z):
     # SIGMOIDGRADIENT returns the gradient of the sigmoid function evaluated at z
 
     sigmo = sigmoid(z)
-    # Compute the gradient of the sigmoid function evaluated at
-    # each value of z.
-    g = sigmo * (1 - sigmo)
-    
-    return g
-
+    return sigmo * (1 - sigmo)
 
 
 
