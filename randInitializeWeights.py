82657f9c91a1791496f00692e6e2448f5e039c62
peaisge <raphi.lescot@hotmail.fr>
2017-11-13 19:09:35 +0100

    Update randInitializeWeights.py

--- randInitializeWeights.py
+++ randInitializeWeights.py
@@ -8,11 +8,8 @@ def randInitializeWeights(layers):
     Theta = []
     for i in range(num_of_layers-1):
         
-        # ====================== TODO ======================
-        # Instructions: Initialize W randomly so that we break the symmetry while
-        #               training the neural network.
+        # Initialize W randomly so that we break the symmetry while training the neural network.
         
-        #W = epsilon * (2 * np.random.rand(layers[i+1], layers[i] + 1) - 1)   
         W = np.random.uniform(-epsilon, epsilon, (layers[i+1], layers[i] + 1))
         Theta.append(W)
                 
